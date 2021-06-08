f661a321113804b3bf0a5ecbb7644db95d5dd729
Sidney Crandall <sdncrandall5@gmail.com>
2021-06-08 14:21:57 -0500

    Fixed Errors and add a few comments

--- HumanPlayer.cs
+++ HumanPlayer.cs
@@ -10,8 +10,8 @@ namespace ShootingDice
         public override int Roll()
         {
             // Propmt the player to choose a number
-            Console.Write($"Welcome, {Name}! What's the number of your Roll? (Any number between 1-15): ");
-            // Parse the number choice into a number for Human Players roll.
+            Console.Write($"Welcome, {Name}! What's the number of your Roll? (Any number between 1-6): ");
+            // Parse the choice into a number for Human Players roll.
             int myRoll = int.Parse(Console.ReadLine());
             return myRoll; 
         } 

821d67ebbd15f34a45871f8af08efacee42a3b05
Sidney Crandall <sdncrandall5@gmail.com>
2021-06-08 11:44:04 -0500

    SmackTalkingPlayer added

--- SmackTalkingPlayer.cs
+++ SmackTalkingPlayer.cs
@@ -1,10 +1,20 @@
+using System;
+
 namespace ShootingDice
 {
     // TODO: Complete this class
 
     // A Player who shouts a taunt every time they roll dice
-    public class SmackTalkingPlayer
+    public class SmackTalkingPlayer : Player
     {
-        public string Taunt { get; }
+        // Random taunt we will give player later
+        public string Taunt { get; set; }
+
+        // Randomly have the character play.
+        public override void Play(Player other)
+        {
+            Console.WriteLine($"{Name} says '{Taunt}!'"); 
+            base.Play(other);
+        }
     }
 }
